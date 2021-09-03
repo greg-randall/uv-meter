1d30cbd98690c9b2b5ca834df717a9379fd13671
Greg Randall <gregrr@gmail.com>
2021-09-02 22:09:49 -0500

    button presses added

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import veml6070
+
+import RPi.GPIO as GPIO
+
+
+from LCD import LCD
+
+
+
+
 import time
 from datetime import datetime, timezone
 
@@ -9,7 +18,15 @@ veml = veml6070.Veml6070()
 
 veml.set_integration_time(veml6070.INTEGRATIONTIME_2T)
 
-f=open("log_uv.txt", "a+")
+lcd = LCD(2,0x27,True)
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#f=open("log_uv.txt", "a+")
 while True:
     utc_time = datetime.fromtimestamp(int(time.time()), timezone.utc)
     local_time = utc_time.astimezone()
@@ -19,8 +36,16 @@ while True:
     uv = veml.get_uva_light_intensity()
     
     output = f"{local_time}, {uv} W/(m*m), {uv_raw}\n"
+    lcd.message(f"{round(uv,3)} W/(m*m)", 1)
+    lcd.message(f"{uv_raw} raw", 2)
     
-    print(output)
+    input_state = GPIO.input(23)
+    if input_state == False:
+        print('Button L')
+        
+    input_state = GPIO.input(24)
+    if input_state == False:
+        print('Button R')
+        
     
-    f.write(output)
-f.close()
+    
