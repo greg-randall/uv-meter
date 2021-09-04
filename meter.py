676a972919760b1e3a5ab5dcc99d8839f80ae960
Greg Randall <gregrr@gmail.com>
2021-09-04 16:12:29 -0500

    going to combine meter with some time experiments i did with carson

--- meter.py
+++ meter.py
@@ -4,58 +4,43 @@ from LCD import LCD
 from pretty_time_delta import pretty_time_delta
 import time
 from datetime import datetime, timezone
+
+desired_uv = 4600
+
+
+
+
+
 veml = veml6070.Veml6070()
 veml.set_integration_time(veml6070.INTEGRATIONTIME_1T)
 lcd = LCD(2,0x27,True)
 
-
- 
-start_measurement_time = time.time()
-start_time = start_measurement_time
 total_uv = 0
-
-desired_uv = 4600
-
-nudge = -0.0004516709999999997
-nudge_factor = 0.000001
-
-average = 1
-
-first_time = True
-
 before = time.time()
 counter = 0
+
+#try to start the loop at the start of a second
+start_time = time.time()
+while round(time.time()) < round(start_time+1):
+    time.sleep(0.0001)
+
 try:
     while True:
-        #if not first_time:
-        #    previous_start = start_time
         start_time = time.time()
+        
         if total_uv >= desired_uv:
             lcd.message(f"Desired Exposure", 1)
             lcd.message(f"Reached!!!", 2)
+            del lcd
+            lcd = LCD(2,0x27,False)
             del lcd
             del veml  
             quit()
 
 
-        
-
-        #if not first_time:
-        #    elapsed = start_time-previous_start
-        #    average = (average + elapsed)/2
-        #    print(f"loop time {elapsed} nudge {nudge}")
-
-        #    if elapsed > 1:
-        #        nudge = nudge - nudge_factor
-        #    elif elapsed < 1:
-        #        nudge = nudge + nudge_factor
-            
-
         uv_raw = veml.get_uva_light_intensity_raw()
         uv = veml.get_uva_light_intensity()
         
-        end_measurement_time = time.time()
-
         total_uv = total_uv + uv
 
         
@@ -70,22 +55,14 @@ try:
                 time_left = pretty_time_delta((desired_uv - total_uv)/uv)
         
 
-
         line_1 = f"{round(total_uv)}/{desired_uv} {round(uv,1)}UV"
         line_2 = f"wait: {time_left} "
         
         lcd.message(line_1, 1)
         lcd.message(line_2, 2)
-        #print(f"{line_1}\n{line_2}")
-
-        #if first_time:
-        #    first_time = False 
+       
 
         counter += 1
-
-        #end_time = time.time()
-        #if end_time-start_time<1:
-        #    time.sleep((1-(end_time-start_time))+nudge)
 
         sleeps = 0
         while round(time.time()) < round(start_time+1):
@@ -98,13 +75,15 @@ try:
 
 
 except KeyboardInterrupt:
-    print(f"average loop time {(time.time()-before)/counter}")
+    print(f"\naverage loop time {(time.time()-before)/counter}\n")
     lcd.message(f"", 1)
     lcd.message(f"", 2)
     del lcd
     lcd = LCD(2,0x27,False)
     del lcd
     del veml
+
+
 
 
 
